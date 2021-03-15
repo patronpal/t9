81ab526c9709021a1e953e113410ba981f49d1e4
patronpal <patron_pal@bk.ru>
2021-03-15 16:51:12 +0300

    123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TaskPageComponent} from './components/task-page/task-page.component';
+import {AllTasksComponent} from './components/all-tasks/all-tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TaskPageComponent},
+  {path: 'login', component: AllTasksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskPageComponent } from './components/task-page/task-page.component';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { AllTasksComponent } from './components/all-tasks/all-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskPageComponent,
+    AllTasksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  exports: [
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
